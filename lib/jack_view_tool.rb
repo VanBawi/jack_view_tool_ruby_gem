9b4eba8c6226e9d8896d5bdf8550d5eb7241b001
Jack <superjackpower85@gmail.com>
2019-11-06 17:59:54 +0800

    reademe edited and gitignore added

---
+++ lib/jack_view_tool.rb
@@ -0,0 +1,7 @@
+require "jack_view_tool/version"
+require "jack_view_tool/renderer"
+
+module JackViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jack_view_tool/version.rb
@@ -0,0 +1,3 @@
+module JackViewTool
+  VERSION = "0.1.0"
+end
